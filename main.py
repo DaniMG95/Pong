fdbccb890d359a559553410f0b952cb79b5a3f47
dani16595 <dani_16_5_95@hotmail.com>
2020-05-20 09:24:03 +0200

    Mensaje de ganador de mejor de 10 rondas

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pygame, random
 
+
 #Variables
 screen_width=1280
 screen_height=960
+velocidad=0
+velocidad_incremento=0.5
+stand_movie=3
+numero_jugadas=10
 
 #Colors
 white_color=(200,200,200)
@@ -29,36 +34,51 @@ score_jugador=0
 score_ia=0
 bola=pygame.Rect(50,10,50,50)
 
-speed=0
-
 speed_bola_x=3
 speed_bola_y=3
+rebote=1
 
 accion="STAND"
+
+def increment_speed():
+    global speed_bola_y,speed_bola_x
+    global rebote
+    if(rebote%2==0):
+        if(speed_bola_x<0):
+            speed_bola_x=speed_bola_x-1
+            speed_bola_y=speed_bola_y-1
+        else:
+            speed_bola_x+=1
+            speed_bola_y+=1
+        rebote=1
+
 
 def draw_puntuacion(jugador,ia):
     global screen
     fuente=pygame.font.Font(None,120)
-    texto_jugador=fuente.render(str(jugador),0,(255,255,255))
-    texto_ia=fuente.render(str(ia),0,(255,255,255))
+    texto_jugador=fuente.render(str(jugador),0,white_color)
+    texto_ia=fuente.render(str(ia),0,white_color)
     screen.blit(texto_jugador,((screen_width//2)-120,40))
     screen.blit(texto_ia,((screen_width//2)+60,40))
 
 def IA():
     ia.top=bola.top-50
 
+
 def mover_rectangulo():
-    global speed
+    global velocidad
     global accion
     if(accion=="UP" and jugador.top>10):
-        jugador.top-=3
+        jugador.top-=stand_movie+velocidad
+        velocidad+=velocidad_incremento
     elif(accion=="DOWN" and jugador.bottom+10< screen_height):
-        jugador.top+=3
+        jugador.top+=stand_movie+velocidad
+        velocidad+=velocidad_incremento
     else:
         jugador.top=jugador.top
+        velocidad=0
 
 def controller(event,pygame):
-    global speed
     global accion
     # Controla el cambio de  las direcciones
     # Orientaciones
@@ -74,33 +94,62 @@ def controller(event,pygame):
     else:
         accion="STAND"
 
-def start_bola():
+def reset_bola():
     global speed_bola_x,speed_bola_y
+    global rebote
+    rebote=1
+    speed_bola_x=3
+    speed_bola_y=3
+    bola.top=screen_height//2
+    bola.left=screen_width//2
+    speed_bola_x=3*random.choice((1,-1))
+    speed_bola_y=3*random.choice((1,-1))
+
+
+def start_bola():
     global score_jugador
     global score_ia
     if(bola.right>screen_width):
+        reset_bola()
         score_jugador+=1
-        bola.top=screen_height//2
-        bola.left=screen_width//2
-        speed_bola_x=3*random.choice((1,-1))
-        speed_bola_y=3*random.choice((1,-1))
     elif(bola.left<0):
+        reset_bola()
         score_ia+=1
-        bola.top=screen_height//2
-        bola.left=screen_width//2
-        speed_bola_x=3*random.choice((1,-1))
-        speed_bola_y=3*random.choice((1,-1))
+
+
+def fin_juego(jugador,ia):
+    global speed_bola_y
+    global speed_bola_x
+    global stand_movie
+    global velocidad_incremento
+    if(jugador==numero_jugadas or ia==numero_jugadas):
+        speed_bola_y=0
+        speed_bola_x=0
+        velocidad_incremento=0
+        stand_movie=0
+    if(jugador==numero_jugadas):
+        fuente=pygame.font.Font(None,120)
+        texto_jugador=fuente.render("!!!HAS GANADO¡¡¡",0,white_color)
+        screen.blit(texto_jugador,((screen_width//2)-300,(screen_height//2)-100))
+    elif(ia==numero_jugadas):
+        fuente=pygame.font.Font(None,120)
+        texto_jugador=fuente.render("!!!HAS PERDIDO¡¡¡",0,white_color)
+        screen.blit(texto_jugador,((screen_width//2)-300,(screen_height//2)-100))
+
 
 
 
 def mover_bola():
     global speed_bola_x,speed_bola_y
+    global rebote
     if(bola.top+50>screen_height or bola.top<0):
         speed_bola_x=-speed_bola_x
 
     if bola.left<20 and jugador.top<bola.top<jugador.top+140:
+        rebote+=1
         speed_bola_y=-speed_bola_y
     if bola.right>screen_width-20 and ia.top<bola.top<ia.top+140:
+        rebote+=1
         speed_bola_y=-speed_bola_y
 
     start_bola()
@@ -110,11 +159,12 @@ def mover_bola():
 
 while True:
     screen.fill(light_gray)
-
+    fin_juego(score_jugador,score_ia)
     for event in pygame.event.get():
         controller(event,pygame)
     mover_bola()
     mover_rectangulo()
+    increment_speed()
     IA()
     pygame.draw.rect(screen,white_color,jugador)
     pygame.draw.ellipse(screen,white_color,bola)
